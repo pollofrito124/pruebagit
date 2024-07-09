c56e933fd4241bc7e0f02b8c72a8236beff0fde6
pollofrito124 <pollofritoconaceite99@gmail.com>
2024-07-09 12:29:30 -0500

    probandopat

---
+++ src/calculadora2/Calculadora2.java
@@ -0,0 +1,29 @@
+
+package calculadora2;
+
+import java.util.Scanner;
+
+
+public class Calculadora2 {
+
+    
+    public static void main(String[] args) {
+       Scanner h = new  Scanner(System.in);
+       //declaro variables 
+      /*double val1;
+      double val2;
+         
+        System.out.println("ingrese un numero");
+        val1 = h.nextDouble();
+        System.out.println("ingresa otro numero");
+        val2 = h.nextDouble();
+        double operacion = val1 * val2;
+        System.out.println("el resultado de multiplicar es "+ operacion);*/
+      
+    
+    }
+}
+                  
+        
+ 
+
